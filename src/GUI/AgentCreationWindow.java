7074ddf422eb2feed5dcd287b5a917fa2f1f8488
lazyfucker <matthewkusaj@gmail.com>
2016-03-15 18:43:27 +0000

    Added Constructor to AgentCreationWindow. Also test for pulling onto my laptop.

--- src/GUI/AgentCreationWindow.java
+++ src/GUI/AgentCreationWindow.java
@@ -5,11 +5,30 @@
  */
 package GUI;
 
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /**
  *
  * @author Matthew
  */
-public class AgentCreationWindow {
+public class AgentCreationWindow extends Window{
     
+    private JPanel panel;
+    
+    private JTextField name;
+    private JButton accept;
+    
+    public AgentCreationWindow (String tit, int width, int height)
+	{
+		super(tit, width, height);
+        }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
